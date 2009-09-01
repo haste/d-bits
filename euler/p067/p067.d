0f99061e12a18c2c165c39f09c71dbc49a9f3bb9
Trond A Ekseth <troeks@gmail.com>
2009-09-01 19:12:02 +0200

    Solution to Project Euler #67.

---
+++ euler/p067/p067.d
@@ -0,0 +1,32 @@
+import tango.io.Stdout,
+	   tango.io.device.File;
+
+import Integer = tango.text.convert.Integer;
+import Text = tango.text.Util;
+
+void main()
+{
+	File file = new File ("triangle.txt");
+	char[] src = new char [file.length];
+	file.read(src);
+	file.close;
+
+	char[][] lines = Text.splitLines(src);
+	int[100][101] data;
+
+	for(int i; i < lines.length; i++)
+	{
+		char[][] line = Text.split(lines[i], " ");
+		for(int j; j < line.length; j++)
+			data[i][j] = Integer.toInt(line[j]);
+	}
+
+	for(int i = data.length - 2; i >= 0; --i)
+		for(int j = 0; j <= i; ++j)
+		{
+			int v = (data[i+1][j] < data[i+1][j+1]);
+			data[i][j] += data[i+1][j+v];
+		}
+
+	Stdout(data[0][0]).newline;
+}
